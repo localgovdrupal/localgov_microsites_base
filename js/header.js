69aeb63875f1cc53b7e717bf7740368cb61ccce6
Mark Conroy <mark@mark.ie>
2022-04-22 14:07:51 +0200

    initial commit of off canvas region

---
+++ js/header.js
@@ -0,0 +1,52 @@
+(function headerScript(Drupal) {
+  Drupal.behaviors.micrositeHeader = {
+    attach: function (context, settings) {
+      context = context || document;
+
+      const offCanvas = context.querySelector("#off-canvas");
+      const offCanvasToggle = context.querySelector(
+        ".microsite-header__off-canvas-toggle"
+      );
+
+      function toggleStates() {
+        toggleState = offCanvasToggle.getAttribute("aria-expanded");
+        offCanvasState = offCanvas.getAttribute("data-expanded");
+      }
+
+      function handleOffCanvas() {
+        toggleStates();
+        if (toggleState === 'true' || offCanvasState === 'true') {
+          handleCloseOffCanvas();
+        } else {
+          handleOpenOffCanvas();
+        }
+      }
+
+      function handleOpenOffCanvas() {
+        toggleStates();
+        offCanvasToggle.setAttribute("aria-expanded", "true");
+        offCanvas.setAttribute("data-expanded", "true");
+        const focusable = offCanvas.querySelectorAll('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])');
+        focusable[0].focus();
+      }
+
+      function handleCloseOffCanvas() {
+        toggleStates();
+        offCanvasToggle.focus();
+        offCanvasToggle.setAttribute("aria-expanded", "false");
+        offCanvas.setAttribute("data-expanded", "false");
+      }
+
+      if (!offCanvasToggle) {
+        return;
+      } else {
+        offCanvasToggle.addEventListener("click", handleOffCanvas);
+        window.addEventListener("keyup", function (e) {
+          if (e.code === "Escape") {
+            handleCloseOffCanvas();
+          }
+        });
+      }
+    },
+  };
+})(Drupal);

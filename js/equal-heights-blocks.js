8479bd15f58dea14bdc7d10f3480d8cdab5d68c6
Maria Young <maria@agile.coop>
2022-09-07 14:13:21 +0100

    Update js to target different paragraphs

--- js/equal-heights-blocks.js
+++ js/equal-heights-blocks.js
@@ -2,13 +2,11 @@
   Drupal.behaviors.equalHeightsBlocks = {
     attach: function (context) {
       context = context || document;
-     
+
       const layouts = Array.from(context.querySelectorAll('.layout'));
       const layoutsWithIaBlocks = layouts.filter(item => item.querySelector('.ia-block'));
-      const layoutsWithBoxLinks = layouts.filter(item => item.querySelector('.box-links-listing__list-item'));
-      const layoutsWithLinkBlocks = layouts.filter(item => item.querySelector('.link-block'));
-      const layoutsWithNewsListBlocks = layouts.filter(item => item.querySelector('.news-list'));
-      const layoutsWithTextBlocks = layouts.filter(item => item.querySelector('.paragraph--type--localgov-text'));
+      const layoutsWithCallOutBox = layouts.filter(item => item.querySelector('.call-out-box'));
+      const layoutsWithTeaserBlocks = layouts.filter(item => item.querySelector('.featured-teaser'));
 
       function equaliseHeightsOfTheseBlocks(layoutWithBlocks, typeOfBlock) {
         layoutWithBlocks.forEach(item => {
@@ -29,26 +27,18 @@
         })
       }
 
-      layoutsWithNewsListBlocks.forEach(layout => {
-        equaliseHeightsOfTheseBlocks(layoutsWithNewsListBlocks, '.news-list');
+      layoutsWithCallOutBox.forEach(layout => {
+        equaliseHeightsOfTheseBlocks(layoutsWithCallOutBox, '.call-out-box');
       });
 
       layoutsWithIaBlocks.forEach(layout => {
         equaliseHeightsOfTheseBlocks(layoutsWithIaBlocks, '.ia-block');
       });
 
-      layoutsWithBoxLinks.forEach(layout => {
-        equaliseHeightsOfTheseBlocks(layoutsWithBoxLinks, '.box-links-listing__list-item');
+      layoutsWithTeaserBlocks.forEach(layout => {
+        equaliseHeightsOfTheseBlocks(layoutsWithTeaserBlocks, '.featured-teaser');
       });
-      
-      layoutsWithLinkBlocks.forEach(layout => {
-        equaliseHeightsOfTheseBlocks(layoutsWithLinkBlocks, '.link-block');
-      });
-      
-      layoutsWithTextBlocks.forEach(layout => {
-        equaliseHeightsOfTheseBlocks(layoutsWithTextBlocks, '.paragraph--type--localgov-text');
-      });
-      
+
     }
   };
 }(Drupal));

419a54ca8f897416ce1be49cb74b9429456559a8
Mark Conroy <mark@annertech.com>
2022-10-18 12:38:30 +0200

    check for offCanvas before processing it

--- js/header.js
+++ js/header.js
@@ -7,77 +7,81 @@
       const offCanvasToggle = context.querySelector(
         ".microsite-header__off-canvas-toggle"
       );
-      const offCanvasClose = context.querySelector('.off-canvas__close');
-      const focusable = offCanvas.querySelectorAll('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])');
+      const offCanvasClose = context.querySelector(".off-canvas__close");
 
-      function toggleStates() {
-        toggleState = offCanvasToggle.getAttribute("aria-expanded");
-        offCanvasState = offCanvas.getAttribute("data-expanded");
-      }
+      if (offCanvas) {
+        const focusable = offCanvas.querySelectorAll(
+          'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+        );
 
-      function handleOffCanvas() {
-        toggleStates();
-        if (toggleState === 'true' || offCanvasState === 'true') {
-          handleCloseOffCanvas();
-        } else {
-          handleOpenOffCanvas();
+        function toggleStates() {
+          toggleState = offCanvasToggle.getAttribute("aria-expanded");
+          offCanvasState = offCanvas.getAttribute("data-expanded");
         }
-      }
 
-      function handleOpenOffCanvas() {
-        toggleStates();
-        offCanvasToggle.setAttribute("aria-expanded", "true");
-        offCanvas.setAttribute("data-expanded", "true");
-        focusable[0].focus();
-      }
-
-      function handleCloseOffCanvas() {
-        toggleStates();
-        offCanvasToggle.focus();
-        offCanvasToggle.setAttribute("aria-expanded", "false");
-        offCanvas.setAttribute("data-expanded", "false");
-      }
-
-      function trapFocusInOffCanvasArea(element) {
-        const firstFocusableElement = focusable[0];  
-        const lastFocusableElement = focusable[focusable.length - 1];
-      
-        element.addEventListener('keydown', function(e) {
-          const isTabPressed = (e.code === 'Tab');
-          if (!isTabPressed) { 
-            return; 
+        function handleOffCanvas() {
+          toggleStates();
+          if (toggleState === "true" || offCanvasState === "true") {
+            handleCloseOffCanvas();
+          } else {
+            handleOpenOffCanvas();
           }
-          /* shift + tab */
-          if ( e.shiftKey ) {
-            if (context.activeElement === firstFocusableElement) {
-              lastFocusableElement.focus();
+        }
+
+        function handleOpenOffCanvas() {
+          toggleStates();
+          offCanvasToggle.setAttribute("aria-expanded", "true");
+          offCanvas.setAttribute("data-expanded", "true");
+          focusable[0].focus();
+        }
+
+        function handleCloseOffCanvas() {
+          toggleStates();
+          offCanvasToggle.focus();
+          offCanvasToggle.setAttribute("aria-expanded", "false");
+          offCanvas.setAttribute("data-expanded", "false");
+        }
+
+        function trapFocusInOffCanvasArea(element) {
+          const firstFocusableElement = focusable[0];
+          const lastFocusableElement = focusable[focusable.length - 1];
+
+          element.addEventListener("keydown", function (e) {
+            const isTabPressed = e.code === "Tab";
+            if (!isTabPressed) {
+              return;
+            }
+            /* shift + tab */
+            if (e.shiftKey) {
+              if (context.activeElement === firstFocusableElement) {
+                lastFocusableElement.focus();
                 e.preventDefault();
               }
             } else {
-            /* tab */ 
-            if (context.activeElement === lastFocusableElement) {
-              firstFocusableElement.focus();
+              /* tab */
+              if (context.activeElement === lastFocusableElement) {
+                firstFocusableElement.focus();
                 e.preventDefault();
               }
             }
+          });
+        }
+
+        if (!offCanvasToggle) {
+          return;
+        } else {
+          offCanvasToggle.addEventListener("click", handleOffCanvas);
+          window.addEventListener("keyup", function (e) {
+            if (e.code === "Escape") {
+              handleCloseOffCanvas();
+            }
+          });
+          trapFocusInOffCanvasArea(offCanvas);
+        }
+        offCanvasClose.addEventListener("click", () => {
+          handleCloseOffCanvas();
         });
       }
-
-      if (!offCanvasToggle) {
-        return;
-      } else {
-        offCanvasToggle.addEventListener("click", handleOffCanvas);
-        window.addEventListener("keyup", function (e) {
-          if (e.code === "Escape") {
-            handleCloseOffCanvas();
-          }
-        });
-        trapFocusInOffCanvasArea(offCanvas);
-      }
-      offCanvasClose.addEventListener('click', () => {
-        handleCloseOffCanvas();
-      });
-
     },
   };
 })(Drupal);
